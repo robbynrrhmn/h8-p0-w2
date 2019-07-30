e48da9e008ef893865224dd14efc00a8bb58fa3b
Robby Nurrahman <robbynrrhmn58@gmail.com>
2019-07-30 19:22:30 +0700

    exercise 3 w2

---
+++ switchcase.js
@@ -0,0 +1,49 @@
+var tanggal = 3; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 6; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 1996; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+switch (tanggal <= 31 && tanggal > 0 && tahun <= 2200 && bulan <= 12 && bulan > 0 && tahun >= 1900) {
+    case true:
+        switch (bulan) {
+            case 1:
+                console.log(tanggal + ' Januari ' + tahun);
+                break;
+            case 2:
+                console.log(tanggal + ' Februari ' + tahun);
+                break;
+            case 3:
+                console.log(tanggal + ' Maret ' + tahun);
+                break;
+            case 4:
+                console.log(tanggal + ' April ' + tahun);
+                break;
+            case 5:
+                console.log(tanggal + ' Mei ' + tahun);
+                break;
+            case 6:
+                console.log(tanggal + ' Juni ' + tahun);
+                break;
+            case 7:
+                console.log(tanggal + ' Juli ' + tahun);
+                break;
+            case 8:
+                console.log(tanggal + ' Agustus ' + tahun);
+                break;
+            case 9:
+                console.log(tanggal + ' September ' + tahun);
+                break;
+            case 10:
+                console.log(tanggal + ' Oktober ' + tahun);
+                break;
+            case 11:
+                console.log(tanggal + ' November ' + tahun);
+                break;
+            case 12:
+                console.log(tanggal + ' Desember ' + tahun);
+                break;
+        }
+        break;
+    default:
+        console.log('input salah');
+        break;
+}
